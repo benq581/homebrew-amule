4f0e4838b1ed71cec7649a8811fdab6907c7354c
Leo Liu <553138+benq581@users.noreply.github.com>
2020-12-03 05:45:55 +0800

    wxmac: disable shared

---
+++ wxmac.rb
@@ -0,0 +1,55 @@
+class Wxmac < Formula
+  desc "Cross-platform C++ GUI toolkit (wxWidgets for macOS)"
+  homepage "https://www.wxwidgets.org"
+  url "https://github.com/wxWidgets/wxWidgets/releases/download/v3.0.5.1/wxWidgets-3.0.5.1.tar.bz2"
+  sha256 "440f6e73cf5afb2cbf9af10cec8da6cdd3d3998d527598a53db87099524ac807"
+  license "wxWindows"
+  head "https://github.com/wxWidgets/wxWidgets.git"
+  
+  depends_on "jpeg"
+  depends_on "libpng"
+  depends_on "libtiff"
+  
+  def install
+    args = [
+      "--prefix=#{prefix}",
+      "--enable-clipboard",
+      "--enable-controls",
+      "--enable-dataviewctrl",
+      "--enable-display",
+      "--enable-dnd",
+      "--enable-graphics_ctx",
+      "--enable-std_string",
+      "--enable-svg",
+      "--enable-unicode",
+      "--enable-webkit",
+      "--enable-webview",
+      "--with-expat",
+      "--with-libjpeg",
+      "--with-libpng",
+      "--with-libtiff",
+      "--with-opengl",
+      "--with-osx_cocoa",
+      "--with-zlib",
+      "--disable-precomp-headers",
+      "--disable-shared",
+      # This is the default option, but be explicit
+      "--disable-monolithic",
+      # Set with-macosx-version-min to avoid configure defaulting to 10.5
+      "--with-macosx-version-min=#{MacOS.version}",
+    ]
+
+    system "./configure", *args
+    system "make", "install"
+
+    # wx-config should reference the public prefix, not wxmac's keg
+    # this ensures that Python software trying to locate wxpython headers
+    # using wx-config can find both wxmac and wxpython headers,
+    # which are linked to the same place
+    inreplace "#{bin}/wx-config", prefix, HOMEBREW_PREFIX
+  end
+
+  test do
+    system bin/"wx-config", "--libs"
+  end
+end
