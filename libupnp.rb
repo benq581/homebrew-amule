5bd60a7ef972a2c9ec7510d287b2267ae38641ad
Leo Liu <553138+benq581@users.noreply.github.com>
2020-12-03 13:42:37 +0800

    libupnp: rollback to 1.12.1

--- libupnp.rb
+++ libupnp.rb
@@ -1,16 +1,15 @@
 class Libupnp < Formula
   desc "Portable UPnP development kit"
   homepage "https://pupnp.sourceforge.io/"
-  url "https://github.com/pupnp/pupnp/releases/download/release-1.14.0/libupnp-1.14.0.tar.bz2"
-  sha256 "ecb23d4291968c8a7bdd4eb16fc2250dbacc16b354345a13342d67f571d35ceb"
-  license "BSD-3-Clause"
+  url "https://github.com/pupnp/pupnp/releases/download/release-1.12.1/libupnp-1.12.1.tar.bz2"
+  sha256 "fc36642b1848fe5a81296d496291d350ecfc12b85fd0b268478ab230976d4009"
 
   def install
     args = %W[
       --disable-debug
       --disable-dependency-tracking
       --prefix=#{prefix}
-      --disable-ipv6
+      --enable-ipv6
     ]
 
     system "./configure", *args
