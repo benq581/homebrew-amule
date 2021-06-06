4f0b4ac20f161f6b674f9ffa8f8901f42b5aa9e0
Leo Liu <553138+benq581@users.noreply.github.com>
2021-06-06 13:33:02 +0800

    cryptopp: update to 8.5.0

--- cryptopp.rb
+++ cryptopp.rb
@@ -1,9 +1,9 @@
 class Cryptopp < Formula
   desc "Free C++ class library of cryptographic schemes"
   homepage "https://www.cryptopp.com/"
-  url "https://www.cryptopp.com/cryptopp840.zip"
-  version "8.4.0"
-  sha256 "c0f5e5cd2c693c8160e9c51666e95949a1c19fd4fe4fef874af2ec1e42757b9a"
+  url "https://www.cryptopp.com/cryptopp850.zip"
+  version "8.5.0"
+  sha256 "95fc50d59488ebf61a735cce2b2ec2c2561fc682077c7b496273d65a1ed93d9e"
  
   # https://cryptopp.com/wiki/Config.h#Options_and_Defines
 
