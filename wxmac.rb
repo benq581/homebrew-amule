49bece029ca0adb2d7a4afcad0d9f4304ff5a502
Leo <553138+benq581@users.noreply.github.com>
2025-02-19 20:18:18 +0800

    wxmac: update to 3.2.6

--- wxmac.rb
+++ wxmac.rb
@@ -1,18 +1,26 @@
 class Wxmac < Formula
-  desc "Cross-platform C++ GUI toolkit (wxWidgets for macOS)"
+  desc "Cross-platform C++ GUI toolkit"
   homepage "https://www.wxwidgets.org"
-  url "https://github.com/wxWidgets/wxWidgets/releases/download/v3.0.5.1/wxWidgets-3.0.5.1.tar.bz2"
-  sha256 "440f6e73cf5afb2cbf9af10cec8da6cdd3d3998d527598a53db87099524ac807"
-  license "wxWindows"
-  head "https://github.com/wxWidgets/wxWidgets.git"
-  
-  depends_on "jpeg"
+  url "https://github.com/wxWidgets/wxWidgets/releases/download/v3.2.6/wxWidgets-3.2.6.tar.bz2"
+  sha256 "939e5b77ddc5b6092d1d7d29491fe67010a2433cf9b9c0d841ee4d04acb9dce7"
+  license "LGPL-2.0-or-later" => { with: "WxWindows-exception-3.1" }
+  head "https://github.com/wxWidgets/wxWidgets.git", branch: "master"
+
+  depends_on "pkgconf" => :build
+  depends_on "jpeg-turbo"
   depends_on "libpng"
   depends_on "libtiff"
-  
+  depends_on "pcre2"
+
+  uses_from_macos "expat"
+  uses_from_macos "zlib"
+
   def install
+    # Remove all bundled libraries excluding `nanosvg` which isn't available as formula
+    %w[catch pcre].each { |l| rm_r(buildpath/"3rdparty"/l) }
+    %w[expat jpeg png tiff zlib].each { |l| rm_r(buildpath/"src"/l) }
+
     args = [
-      "--prefix=#{prefix}",
       "--enable-clipboard",
       "--enable-controls",
       "--enable-dataviewctrl",
@@ -22,31 +30,42 @@ class Wxmac < Formula
       "--enable-std_string",
       "--enable-svg",
       "--enable-unicode",
-      "--enable-webkit",
-      "--enable-webview",
+      "--enable-webviewwebkit",
       "--with-expat",
+      "--with-libiconv",
       "--with-libjpeg",
       "--with-libpng",
       "--with-libtiff",
+      # Set with-macosx-version-min to avoid configure defaulting to 10.5
+      "--with-macosx-version-min=#{MacOS.version}",
       "--with-opengl",
       "--with-osx_cocoa",
       "--with-zlib",
-      "--disable-precomp-headers",
-      "--disable-shared",
       # This is the default option, but be explicit
       "--disable-monolithic",
-      # Set with-macosx-version-min to avoid configure defaulting to 10.5
-      "--with-macosx-version-min=#{MacOS.version}",
+      "--disable-precomp-headers",
+      "--disable-shared",
+      "--disable-tests",
     ]
 
-    system "./configure", *args
+    if OS.mac?
+      # Work around deprecated Carbon API, see
+      # https://github.com/wxWidgets/wxWidgets/issues/24724
+      inreplace "src/osx/carbon/dcscreen.cpp", "#if !wxOSX_USE_IPHONE", "#if 0" if MacOS.version >= :sequoia
+    end
+
+    system "./configure", *args, *std_configure_args
     system "make", "install"
 
-    # wx-config should reference the public prefix, not wxmac's keg
+    # wx-config should reference the public prefix, not wxwidgets's keg
     # this ensures that Python software trying to locate wxpython headers
-    # using wx-config can find both wxmac and wxpython headers,
+    # using wx-config can find both wxwidgets and wxpython headers,
     # which are linked to the same place
-    inreplace "#{bin}/wx-config", prefix, HOMEBREW_PREFIX
+    inreplace bin/"wx-config", prefix, HOMEBREW_PREFIX
+
+    # For consistency with the versioned wxwidgets formulae
+    bin.install_symlink bin/"wx-config" => "wx-config-#{version.major_minor}"
+    (share/"wx"/version.major_minor).install share/"aclocal", share/"bakefile"
   end
 
   test do
